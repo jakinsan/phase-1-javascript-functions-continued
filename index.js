46a2e2aa8321580c5536ac6d2ab847f7b9d4e7d6
James Akinsanya <j.akins24@gmail.com>
2023-03-20 22:52:32 -0400

    commit tree 16e130c07831da86fe70d6aca6af7b85be219d1f

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// code your solution here
+
+// function (){
+//     console.log("You have been razzled!")    
+// }
+// razzle(); 
+
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`); 
+}
+
+
+// function saturdayFun(); {
+//     if () {
+//         return "This Saturday, I want to" 
+//     } else {
+//         return 
+//     }
+
+// const fn = function mondayWork(activity = "go to the office") 
+
+// fn();
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`; 
+}
+
+
+function wrapAdjective(prompt = "*"){
+    function result(adjective){
+        return `You are ${prompt}${adjective}${prompt}!`
+    }
+    return result
+}
+
+
+// function wrapAdjective(parameter = "*") {
+//     const part1 = "You are"
+//     return function innerFunction(paremeter2 = "special") {
+// return `${part1} ${parameter}${paremeter2}${parameter}!`
+//     };
+// }
